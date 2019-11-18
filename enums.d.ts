1c07e913e917bc24893abcc2ff1311295ee0b3b5
song2222 <55080197+song2222@users.noreply.github.com>
2019-11-18 13:33:42 +0800

    Update enums.d.ts

--- enums.d.ts
+++ enums.d.ts
@@ -25,27 +25,73 @@
 
 
     declare enum RemoteButton {   
-      CH_MINUS = 0x45,
-	  CH = 0x46,
-	  CH_Add = 0x47,
-	  PREV = 0x44,
-	  PLAY = 0x43,
-	  NUM_200 = 0xd,
-	  NEXT = 0x40,
-	  NUM_100 = 0x19,
-	  Minus = 0x7,
-	  Add = 0x15,
-	  EQ = 0x9,
+          //% block=number1
+      number1 = 0x45,
+
+      //% block=number2
+
+	number2 = 0x46,
+
+      //% block=number3
+
+	  number3 = 0x47,
+
+      //% block=number4
+
+	  number4 = 0x44,
+
+      //% block=number5
+
+	  number5 = 0x40,
+
+      //% block=number6
+
+	  number6 = 0x43,
+
+      //% block=number7
+
+	  number7 = 0x07,
+
+      //% block=number8
+
+	 number8 = 0x15,
+
+      //% block=number9
+
+	  number9 = 0x09,
+
+      //% block=number0
+
+      number0 = 0x19,
+
+	  //% block=#
+
+	  EQ = 0x0d,
+
+      //% block=*
+
 	  NUM0 = 0x16,
-	  NUM1 = 0xc,
-	  NUM2 = 0x18,
-	  NUM3 = 0x5e,
-	  NUM4 = 0x8,
-	  NUM5 = 0x1c,
-	  NUM6 = 0x5a,
-	  NUM7 = 0x42,
-	  NUM8 = 0x52,
-	  NUM9 = 0x4A,
+
+      //% block=up
+
+	  up = 0x18,
+
+      //% block=left
+
+	  left = 0x8,
+
+      //% block=OK
+
+	  OK = 0x1c,
+
+      //% block=right
+
+	 right = 0x5a,
+
+      //% block=down
+
+	  down = 0x52,
+
     }
 declare namespace Mbit_IR {
 }
