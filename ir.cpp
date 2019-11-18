7c44fc04a6f9ceb14ea3e2388894fb6a69fe503e
song2222 <55080197+song2222@users.noreply.github.com>
2019-11-18 14:19:53 +0800

    Update ir.cpp

--- ir.cpp
+++ ir.cpp
@@ -28,35 +28,31 @@ enum class Pins{
 };
 
 enum class RemoteButton {
-    	  	   CH_MINUS = 0x45,
+    	  	   number1 = 0x45,
 
-	  CH = 0x46,
+	  number2 = 0x46,
 
-	  CH_Add = 0x47,
+	  number3 = 0x47,
 
-	  PREV = 0x44,
+	  number4 = 0x44,
 
-	  PLAY = 0x43,
+	  number6 = 0x43,
 
-	  NUM_200 = 0xd,
+	  number11 = 0xd,
 
-	  NEXT = 0x40,
+	  number5 = 0x40,
 
-	  NUM_100 = 0x19,
+	  number0 = 0x19,
 
-	  Minus = 0x7,
+	  number7 = 0x7,
 
-	  Add = 0x15,
+	  number8 = 0x15,
 
-	  EQ = 0x9,
+	  number9 = 0x9,
 
-	  NUM0 = 0x16,
-
-	  NUM1 = 0xc,
+	  NUM10 = 0x16,
 
 	  up = 0x18,
-
-	  NUM3 = 0x5e,
 
 	  left = 0x8,
 
@@ -64,11 +60,7 @@ enum class RemoteButton {
 
 	  right = 0x5a,
 
-	  NUM7 = 0x42,
-
 	  down = 0x52,
-
-	  NUM9 = 0x4A,
 
 };
 
