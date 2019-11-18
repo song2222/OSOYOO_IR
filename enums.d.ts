6cc0189704b66b6b66261527895b12138dda9215
song2222 <55080197+song2222@users.noreply.github.com>
2019-11-18 14:01:29 +0800

    Update enums.d.ts

--- enums.d.ts
+++ enums.d.ts
@@ -25,16 +25,47 @@
 
 
     declare enum RemoteButton {   
+	   CH_MINUS = 0x45,
+
+	  CH = 0x46,
+
+	  CH_Add = 0x47,
+
+	  PREV = 0x44,
+
+	  PLAY = 0x43,
+
+	  NUM_200 = 0xd,
+
+	  NEXT = 0x40,
+
+	  NUM_100 = 0x19,
+
+	  Minus = 0x7,
+
+	  Add = 0x15,
+
+	  EQ = 0x9,
+
 	  NUM0 = 0x16,
-	  NUM1 = 0x45,
-	  NUM2 = 0x46,
-	  NUM3 = 0x47,
-	  NUM4 = 0x44,
-	  NUM5 = 0x40,
-	  NUM6 = 0x43,
-	  NUM7 = 0x07,
-	  NUM8 = 0x15,
-	  NUM9 = 0x09
+
+	  NUM1 = 0xc,
+
+	  up = 0x18,
+
+	  NUM3 = 0x5e,
+
+	  left = 0x8,
+
+	  ok = 0x1c,
+
+	  right = 0x5a,
+
+	  NUM7 = 0x42,
+
+	  down = 0x52,
+
+	  NUM9 = 0x4A,
     }
 declare namespace Mbit_IR {
 }
